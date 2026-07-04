ffcbdd3ab23243aceeca11028106481b4956dc55
Parkie2361 <157774305+Parkie2361@users.noreply.github.com>
2026-07-03 17:42:17 -0700

    Make top bar logo 2.5x bigger (44px -> 110px)

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,7 +5,7 @@ export default function TopBar() {
     <div style={{ maxWidth: 1040, margin: "0 auto", padding: "24px 28px 0" }}>
       <Link href="/" style={{ display: "inline-flex", alignItems: "center", textDecoration: "none", color: "inherit" }}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/logo.png" alt="Potluck Photos" style={{ height: 44, width: "auto", display: "block" }} />
+        <img src="/logo.png" alt="Potluck Photos" style={{ height: 110, width: "auto", display: "block" }} />
       </Link>
     </div>
   );
